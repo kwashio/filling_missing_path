4955b41f38ca2d7b6b0c058c08558e8753cdd27e
kwashio <kokiwashio@g.ecc.u-tokyo.ac.jp>
2017-12-16 16:00:07 +0900

    update

--- corpus/create_triples.sh
+++ corpus/create_triples.sh
@@ -20,43 +20,9 @@ do
 done
 wait
 
-#
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_aa_parsed" > "paths_aa"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ab_parsed" > "paths_ab"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ac_parsed" > "paths_ac"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ad_parsed" > "paths_ad"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ae_parsed" > "paths_ae"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_af_parsed" > "paths_af"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ag_parsed" > "paths_ag"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ah_parsed" > "paths_ah"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ai_parsed" > "paths_ai"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_aj_parsed" > "paths_aj"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ak_parsed" > "paths_ak"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_al_parsed" > "paths_al"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_am_parsed" > "paths_am"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_an_parsed" > "paths_an"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ao_parsed" > "paths_ao"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "$wiki_dump_file""_ap_parsed" > "paths_ap"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ab_parsed.out" > "paths_ab"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ac_parsed.out" > "paths_ac"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ad_parsed.out" > "paths_ad"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ae_parsed.out" > "paths_ae"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_af_parsed.out" > "paths_af"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ag_parsed.out" > "paths_ag"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ah_parsed.out" > "paths_ah"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ai_parsed.out" > "paths_ai"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_aj_parsed.out" > "paths_aj"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ak_parsed.out" > "paths_ak"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_al_parsed.out" > "paths_al"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_am_parsed.out" > "paths_am"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_an_parsed.out" > "paths_an"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ao_parsed.out" > "paths_ao"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ap_parsed.out" > "paths_ap"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_aq_parsed.out" > "paths_aq"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_ar_parsed.out" > "paths_ar"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_as_parsed.out" > "paths_as"
-#awk -v OFS='\t' '{i[$3]++} END{for(x in i){print x, i[x]}}' "wiki_at_parsed.out" > "paths_at"
 cat paths_a* > paths_temp;
 cat paths_temp | grep -v "$(printf '\t1$')" > frequent_paths_temp;
 awk -F$'\t' '{i[$1]+=$2} END{for(x in i){print x"\t"i[x]}}' frequent_paths_temp > paths;
 awk -F$'\t' '$2 >= 5 {print $1}' paths > frequent_paths
+
+python frequent_path2id.py
---
+++ corpus/frequent_path2id.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+"""
+だいたい５回以上出た頻出pathのid辞書を作成する。
+"""
+
+import pickle
+
+
+def path2id():
+    with open('frequent_paths', 'r') as f:
+        frequent_paths = set([line.strip() for line in f])
+
+    path_to_id = {p: i for i, p in enumerate(list(frequent_paths))}
+    id_to_path = {i: p for i, p in enumerate(list(frequent_paths))}
+
+    del frequent_paths
+
+    with open('path_to_id.dump', 'wb') as f:
+        pickle.dump(path_to_id, f)
+
+    with open('id_to_path.dump','wb') as f:
+        pickle.dump(id_to_path, f)
+
+if __name__ == '__main__':
+    path2id()
