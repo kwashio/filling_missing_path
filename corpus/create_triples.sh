26f072cb89e5632527338899a7d1a161e4743693
kwashio <kokiwashio@g.ecc.u-tokyo.ac.jp>
2017-12-16 15:31:42 +0900

    first commit

---
+++ corpus/create_triples.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+wiki_dump_file=$1
+
+# Parse wikipedia. Splitting to 20 files and running in parallel.
+echo 'Parsing wikipedia...'
+split -nl/20 $wiki_dump_file $wiki_dump_file"_";
+
+for x in {a..t}
+do
+( python parse_wikipedia.py $wiki_dump_file"_a"$x $wiki_dump_file"_a"$x"_parsed\
+" ) &
+done
+wait
